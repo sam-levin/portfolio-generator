fac627148c9e92c56ec438243041064ea720b643
sam-levin <sam.ky.levin@gmail.com>
2022-03-16 10:54:29 -1000

    finishing up capture-input feature

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const profileDataArr = process.argv.slice(2, process.argv.length);
+console.log(profileDataArr);
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+}
+
+
+printProfileData(profileDataArr);
+
